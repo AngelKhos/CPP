7f5e6d64405476a2e204bcad74761baba0e1e6eb
authomas <authomas@student.42lyon.fr>
2026-09-08 17:19:45 +0200

    fini fini

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,6 +1,7 @@
 #include "PmergeMe.hpp"
 #include <cmath>
 #include <ctime>
+#include <iomanip>
 
 std::vector<t_pair> group_to_pair(std::vector<t_group> groups)
 {
@@ -632,8 +633,8 @@ int main(int ac, char **av)
     sort(dlist);
     std::clock_t dend = clock();
 
-    std::cout << "Time to process a range of " << ac - 1 << " elements with std::vector: " << static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
-    std::cout << "Time to process a range of " << ac - 1 << " elements with std::deque: " << static_cast<double>(dend - dstart) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
+    std::cout << "Time to process a range of " << ac - 1 << " elements with std::vector: " << std::fixed << std::setprecision(6) << static_cast<double>(end - start) / CLOCKS_PER_SEC << " us" << std::endl;
+    std::cout << "Time to process a range of " << ac - 1 << " elements with std::deque: " << std::fixed << std::setprecision(6) << static_cast<double>(dend - dstart) / CLOCKS_PER_SEC << " us" << std::endl;
 
     return 0;
 }
